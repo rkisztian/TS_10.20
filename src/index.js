a6002dd8acfd8fa4278f553491193becd35abce9
John Doe <johndoe@example.com>
2022-10-20 14:19:29 +0200

    Létrehoztunk egy Interfacet-t

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 "use strict";
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var _Kony_cim;
 class Kony {
+    constructor(cim) {
+        _Kony_cim.set(this, void 0);
+        __classPrivateFieldSet(this, _Kony_cim, cim, "f");
+    }
+    get cim() { return __classPrivateFieldGet(this, _Kony_cim, "f"); }
+    set cim(cim) { __classPrivateFieldSet(this, _Kony_cim, cim, "f"); }
+    kiir() {
+        console.log(this.toString());
+    }
+    toString() {
+        return __classPrivateFieldGet(this, _Kony_cim, "f");
+    }
 }
+_Kony_cim = new WeakMap();
 class Zene {
     constructor(cim, hossz) {
         this.cim = cim;
@@ -13,5 +38,16 @@ class Zene {
         return this.cim + " (" + this.hossz + " sec.)";
     }
 }
-let SpitItOut = new Zene('Spit it out', 230);
-Sp
+/*let SpitItOut = new Zene('Spit it out', 230);
+SpitItOut.kiir();
+
+let TheLordOfTheRings = new Kony("The Lord of the Rings");
+TheLordOfTheRings.kiir();
+*/
+let termekek = [
+    new Zene('Spit it out', 230),
+    new Kony("The Lord of the Rings")
+];
+for (let termek of termekek) {
+    termek.kiir();
+}
--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,32 @@ interface Multimedia{
     toString() : string;
 }
 
+
 class Kony implements Multimedia{
-    
+    #cim : string;
+
+    constructor(cim : string){
+        this.#cim = cim;
+    }
+
+    get cim() {return this.#cim;}
+    set cim(cim : string){this.#cim = cim;}
+
+    kiir(): void {
+        console.log(this.toString());
+        
+    }
+
+    toString(): string {
+        return this.#cim;
+    }
 }
+
 
 class Zene implements Multimedia{
     cim : string;
     hossz: number;
+
     kiir(): void{
         console.log(this.toString());
     }
@@ -24,4 +43,29 @@ class Zene implements Multimedia{
     }
 
     
+
+    toString() : string{
+        return this.cim + " (" + this.hossz + " sec.)";
+    }
+
+}
+
+
+/*let SpitItOut = new Zene('Spit it out', 230);
+SpitItOut.kiir();
+
+let TheLordOfTheRings = new Kony("The Lord of the Rings");
+TheLordOfTheRings.kiir();
+*/
+
+let termekek : Multimedia[] = [
+    new Zene('Spit it out', 230)
+    new Kony("The Lord of the Rings")
+];
+
+
+for(let termek of termekek){
+    termek.kiir();
+}
+
 
